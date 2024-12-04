b928c8f055daa451480307dd16f54bfac4295311
anshul <anshulkumar123.iimt@gmail.com>
2024-12-04 10:42:19 +0530

    first commit

---
+++ src/data/contants.ts
@@ -0,0 +1,69 @@
+export const PRODUCTS_LIST: Product[] = [
+    {
+      id: '1',
+      name: 'APPLE iPhone 14 (Blue, 128 GB)',
+      imageUrl:
+      http://surl.li/gwidax
+      originalPrice: 79990,
+      discountPrice: 65999,
+      offerPercentage: 17,
+      rating: 4.7,
+      ratingCount: 8794,
+      tags: [
+        '12MP Front Camera',
+        '12MP Dual Rear Camera',
+        '15.49 cm (6.1 inch) Super Retina XDR Display',
+
+        id: '2',
+      name: 'Samsung s24 ultra (Starlight, 256 GB)',
+      imageUrl:
+      http://surl.li/rkxnqw
+      originalPrice: 1,65,999,
+      discountPrice: 1,59,999,
+      offerPercentage: 15,
+      rating: 4.7,
+      ratingCount: 7999,
+      tags: ['200MP + 50MP + 12MP + 10MP | 17.27 cm (6.8 inch) Quad HD+ Display'],
+      ],
+    },
+    {
+      id: '2',
+      name: 'Samsung s24 ultra (Starlight, 256 GB)',
+      imageUrl:
+      http://surl.li/rkxnqw
+      originalPrice: 1,65,999,
+      discountPrice: 1,59,999,
+      offerPercentage: 15,
+      rating: 4.7,
+      ratingCount: 7999,
+      tags: ['200MP + 50MP + 12MP + 10MP | 17.27 cm (6.8 inch) Quad HD+ Display'],
+    },
+    {
+      id: '3',
+      name: 'Samsung S23 Ultra (Purple, 128 GB)',
+      imageUrl:
+      http://surl.li/zdhkkdg',
+      originalPrice: 75999,
+      discountPrice: 1,49,999,
+      offerPercentage: 49%,
+      rating: 4.7,
+      ratingCount: 8794,
+      tags: ['200MP + 50MP + 12MP + 10MP | 16.9 cm 500 Pixels Per Inch (PPI)'],
+    },
+    {
+      id: '4',
+      name: 'APPLE iPhone 11 (White, 64 GB)',
+      imageUrl:
+      http://surl.li/skecso
+      discountPrice: 38999,
+      offerPercentage: 11,
+      rating: 4.6,
+      ratingCount: 180810,
+      tags: [
+        '12MP Front Camera',
+        '12MP Dual Rear Camera',
+        '15.49 cm (6.1 inch) Super Retina XDR Display',
+      ],
+    },
+  ];
+  
